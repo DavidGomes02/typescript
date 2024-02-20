d7d565389d71b843988a028ee189fb1f50f3797f
David Gomes <david.henrique4560@gmail.com>
2024-02-20 11:37:16 -0300

    .

---
+++ src/exercicio02.ts
@@ -0,0 +1,29 @@
+class livro {
+    titulo: string;
+    autor: string;
+    editora: string;
+    categoria: string;
+    
+    constructor(titulo: string, autor: string, editora: string, categoria: string){
+        this.titulo = titulo;
+        this.autor = autor;
+        this.editora = editora;
+        this.categoria = categoria;
+
+    }
+}
+class usuario{
+     nome: string;
+     endereco: string;
+     telefone: string;
+
+    constructor(nome: string, endereco: string, telefone: string){
+        this.nome = nome;
+        this.endereco = endereco;
+        this.telefone = telefone;
+    }
+
+}
+class emprestimo{
+    
+}
