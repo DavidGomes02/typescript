665c624ccc9844e722f9b1429cbcd255b7507565
David Gomes <david.henrique4560@gmail.com>
2024-02-21 09:13:39 -0300

    .

--- src/exercicio02.ts
+++ src/exercicio02.ts
@@ -1,10 +1,25 @@
-class livro {
+
+class Usuario {
+    nome: string;
+    endereco: string;
+    telefone: string;
+
+    constructor(nome: string, endereco: string, telefone: string) {
+        this.nome = nome;
+        this.endereco = endereco;
+        this.telefone = telefone;
+    }
+}
+
+const usuario1 = new Usuario('David', 'Av industrial', '84987047031');
+
+class Livro {
     titulo: string;
     autor: string;
     editora: string;
     categoria: string;
-    
-    constructor(titulo: string, autor: string, editora: string, categoria: string){
+
+    constructor(titulo: string, autor: string, editora: string, categoria: string) {
         this.titulo = titulo;
         this.autor = autor;
         this.editora = editora;
@@ -12,18 +27,20 @@ class livro {
 
     }
 }
-class usuario{
-     nome: string;
-     endereco: string;
-     telefone: string;
 
-    constructor(nome: string, endereco: string, telefone: string){
-        this.nome = nome;
-        this.endereco = endereco;
-        this.telefone = telefone;
+const livro1 = new Livro('Dom Casmurro', 'Machado de Assis', 'Principis', 'Romance, ficção, memórias, Realismo literário');
+
+class Emprestimo {
+    dataEmprestimo: Date;
+    dataDevolucao: Date;
+    infoUsuario: Usuario;
+    infoLivro: Livro;
+    constructor(dataEmprestimo: Date, dataDevolucao: Date, infoUsuario: Usuario, infoLivro: Livro) {
+        this.dataEmprestimo = dataEmprestimo;
+        this.dataDevolucao = dataDevolucao;
+        this.infoUsuario = infoUsuario;
+        this.infoLivro = infoLivro
     }
-
 }
-class emprestimo{
-    
-}
+const emprestimo1 = new Emprestimo(new Date('2024.02.21'), new Date('2024.03.21'), usuario1, livro1);
+console.log(emprestimo1);
