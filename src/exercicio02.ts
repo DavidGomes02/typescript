1249c7eb7b07de3b8f6807095ef664b77ad3f48a
David Gomes <david.henrique4560@gmail.com>
2024-02-21 11:43:29 -0300

    .

--- src/exercicio02.ts
+++ src/exercicio02.ts
@@ -9,10 +9,16 @@ class Usuario {
         this.endereco = endereco;
         this.telefone = telefone;
     }
+    mostrarDados(){
+    console.log('---------------Dados do usuario---------------');
+        console.log(`Nome do usuario: ${this.nome}`);
+        console.log(`Endereço: ${this.endereco}`);
+        console.log(`telefone: ${this.telefone}`);
+        }
 }
 
 const usuario1 = new Usuario('David', 'Av industrial', '84987047031');
-
+console.log(usuario1.mostrarDados())
 class Livro {
     titulo: string;
     autor: string;
@@ -26,9 +32,17 @@ class Livro {
         this.categoria = categoria;
 
     }
+    dadosLivros(){
+        console.log('---------------Dados do livro---------------');
+            console.log(`Titulo do livro: ${this.titulo}`);
+            console.log(`Autor: ${this.autor}`);
+            console.log(`Editora: ${this.editora}`);
+            console.log(`Categoria: ${this.categoria}`);
+            }
 }
 
 const livro1 = new Livro('Dom Casmurro', 'Machado de Assis', 'Principis', 'Romance, ficção, memórias, Realismo literário');
+console.log(livro1.dadosLivros())
 
 class Emprestimo {
     dataEmprestimo: Date;
@@ -41,6 +55,13 @@ class Emprestimo {
         this.infoUsuario = infoUsuario;
         this.infoLivro = infoLivro
     }
+    dadosLivros(){
+        console.log('---------------Emprestimo---------------');
+            console.log(`Data do Emprestimo: ${this.dataEmprestimo.toLocaleDateString()}`);
+            console.log(`Data de Devolução: ${this.dataDevolucao.toLocaleDateString()}`);
+            console.log(`Informação do usuario: ${usuario1.nome}`);
+            console.log(`informações do livro: ${livro1.titulo}`);
+            }
 }
 const emprestimo1 = new Emprestimo(new Date('2024.02.21'), new Date('2024.03.21'), usuario1, livro1);
-console.log(emprestimo1);
+console.log(emprestimo1.dadosLivros());
